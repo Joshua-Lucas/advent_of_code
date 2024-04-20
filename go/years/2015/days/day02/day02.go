999d2fff37284b0f398c887b1eb1396fcbea2214
Joshua Lucas <joshua@wholesalegorilla.net>
2024-04-20 15:11:38 -0400

    feat(2015d2): add logic to solve part one

--- go/years/2015/days/day02/day02.go
+++ go/years/2015/days/day02/day02.go
@@ -1,25 +1,24 @@
+// Solution for Advent to Code day 2 of year 2015
+// Puzzle can be found at https://adventofcode.com/2015/day/2
+
 package dayTwo
 
 import (
-	"fmt"
-
 	"joshualucas.dev/aoc/years/2015/days/day02/data"
 )
 
 func RunPartOne(instructions string) int {
+	totalWrappingPaper := 0
 
-	// figure out how to take the string and turn break it down into different gifts.
+	// Prep instructions to iterate over to calculate total wrapping paper.
+	parsedInstructions := data.PrepInstructions(instructions)
 
-	// Create a struct that that holds the gifts l,w,h,smallest side, and surface area
-	// loop through gift list instructions creating an array of structs
-	// then loop through that array of gift structs to calculate the need amount of wrapping paper
-	// return that value
+	// iterate over the instructions and calculate total wrapping needed.
+	for _, present := range parsedInstructions {
+		wrappingNeededForPresent := present.SurfaceArea + present.SmallestSide
 
-	// or
-	aPresent := data.NewPresent(2, 3, 4)
-	// create a value to store total gift surface area
-	// create a function that returns surface needed per gift
-	// return needed amount of wrapping paper.
-	fmt.Println(aPresent)
-	return 1
+		totalWrappingPaper = totalWrappingPaper + wrappingNeededForPresent
+	}
+
+	return totalWrappingPaper
 }
