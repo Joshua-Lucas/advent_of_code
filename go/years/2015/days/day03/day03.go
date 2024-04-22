54fdbe94b3e1d1afd1e2d267932f3c61ec598c5c
Joshua Lucas <joshua@wholesalegorilla.net>
2024-04-22 10:37:37 -0400

    feat(2015d3): add logic to solve part one of day 3

--- go/years/2015/days/day03/day03.go
+++ go/years/2015/days/day03/day03.go
@@ -1,6 +1,70 @@
+// Day 3: Perfectly Spherical Houses in a Vacuum
+// https://adventofcode.com/2015/day/3
+
 package dayThree
 
-func SolvePuzzle(instructions string) (int, int) {
-	return 3, 3
+import (
+	"fmt"
+
+	"joshualucas.dev/aoc/years/2015/days/day03/data"
+)
+
+// SolvePuzzle returns part one and part two solutions for puzzle from Year 2015 Day 3 event.
+func SolvePuzzle(directions string) (int, int) {
+
+	// Create delivery log with the stating delivery of house at 0,0
+	deliveryLog := []data.House{{Longitude: 0, Latitude: 0}}
+
+	// Follow the directions given
+	for _, char := range directions {
+
+		// If there is a newline(\n) char in the directions skip it.
+		if char == 10 {
+			continue
+		}
+
+		//find the last house visited.
+		lastHouse := deliveryLog[len(deliveryLog)-1]
+
+		// Then, get the location of the next house to deliver at.
+		houseDeliveriedAt, err := data.LogDelivery(lastHouse, string(char))
+
+		// If there is an error with the direction we just want to skip it, and log
+		// that we go an error.
+		if err != nil {
+			fmt.Printf("Issue with the directions. Got error: %v", err)
+			continue
+		}
+
+		// Log the delivery of a present at the house.
+		deliveryLog = append(deliveryLog, houseDeliveriedAt)
+
+	}
+
+	// Track how many house received a present.
+	var atleastOnePresentDeliveried []data.House
+
+	// check that the house was not already tracked.
+	for _, house := range deliveryLog {
+		if !SliceContains(atleastOnePresentDeliveried, house) {
+			atleastOnePresentDeliveried = append(atleastOnePresentDeliveried, house)
+		}
+	}
+
+	numberOfHousesDeliveredTo := len(atleastOnePresentDeliveried)
+
+	return numberOfHousesDeliveredTo, 0
 }
 
+// SliceContains returns Boolean telling us if the value was in the slice.
+func SliceContains(slice []data.House, value data.House) bool {
+	houseMatches := false
+	for _, house := range slice {
+		if house.Matches(value) {
+			houseMatches = true
+			break
+		}
+	}
+
+	return houseMatches
+}
--- go/years/2015/days/day03/day03_test.go
+++ go/years/2015/days/day03/day03_test.go
@@ -1,1 +1,99 @@
 package dayThree_test
+
+import (
+	"testing"
+
+	dayThree "joshualucas.dev/aoc/years/2015/days/day03"
+	"joshualucas.dev/aoc/years/2015/days/day03/data"
+)
+
+func TestSolvePuzzle(t *testing.T) {
+	type testCaseDay struct {
+		input    string
+		expected int
+	}
+
+	testCases := map[string]testCaseDay{
+		"did not move": {
+			input:    "",
+			expected: 1,
+		},
+		"move on x axis": {
+			input:    ">",
+			expected: 2,
+		},
+		"move all directions": {
+
+			input:    "^>v<",
+			expected: 4,
+		},
+		"move on y axis": {
+			input:    "^v^v^v^v^v",
+			expected: 2,
+		},
+		"do not move on wrong char": {
+			input:    "\n",
+			expected: 1,
+		},
+	}
+
+	for name, tc := range testCases {
+
+		t.Run(name, func(t *testing.T) {
+			result, _ := dayThree.SolvePuzzle(tc.input)
+
+			if result != tc.expected {
+				t.Errorf("Result: %v, but expected %v from input: %v", result, tc.expected, tc.input)
+			}
+		})
+	}
+}
+
+type inputSliceContains struct {
+	slice []data.House
+	value data.House
+}
+
+func TestSliceContains(t *testing.T) {
+
+	testCases := map[string]struct {
+		input    inputSliceContains
+		expected bool
+	}{
+		"Value is in slice": {
+			input: inputSliceContains{
+				slice: []data.House{
+					{Longitude: 1, Latitude: 2},
+					{Longitude: 2, Latitude: 2},
+					{Longitude: 1, Latitude: 2},
+					{Longitude: 1, Latitude: 3},
+				},
+				value: data.House{Longitude: 1, Latitude: 2},
+			},
+			expected: true,
+		},
+		"Value is not in slice": {
+			input: inputSliceContains{
+				slice: []data.House{
+					{Longitude: 1, Latitude: 2},
+					{Longitude: 2, Latitude: 2},
+					{Longitude: 1, Latitude: 2},
+					{Longitude: 1, Latitude: 3},
+				},
+				value: data.House{Longitude: 3, Latitude: 2},
+			},
+			expected: false,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result := dayThree.SliceContains(tc.input.slice, tc.input.value)
+
+			if result != tc.expected {
+				t.Errorf("Result: %v, but expected %v from input: %v", result, tc.expected, tc.input)
+			}
+
+		})
+	}
+}
