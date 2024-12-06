b88dc1afaa8c1e62947da3f2dadb0535be473269
Joshua Lucas <joshua@wholesalegorilla.net>
2024-12-06 08:41:04 -0500

    refactor(2024): update logic for part one and add logic for part 2

--- rust/src/years/year_2024/days/day02/answer.rs
+++ rust/src/years/year_2024/days/day02/answer.rs
@@ -4,7 +4,7 @@ use anyhow::{anyhow, Result};
 
 use crate::utils::inputs::puzzle_input_string;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Report {
     levels: Vec<i32>,
 }
@@ -27,55 +27,124 @@ impl FromStr for Report {
 
 impl Report {
     fn is_safe(&self) -> bool {
-        let mut is_gradual = true;
-
         // The levels are all increasing
-        let mut is_monotonically_increasing = true;
+        let is_safe = match (self.levels[0], self.levels[1]) {
+            (a, b) if a < b => {
+                let mut safe = true;
+                for (current, next) in self.levels.iter().zip(self.levels.iter().skip(1)) {
+                    if current < next {
+                        // Now check that the increase is gradual
+                        let difference = next - current;
 
-        for (current, next) in self.levels.iter().zip(self.levels.iter().skip(1)) {
-            if current < next {
-                // Now check that the increase is gradual
-                let difference = next - current;
-
-                if difference >= 1 && difference <= 3 {
-                    continue;
-                } else {
-                    is_gradual = false;
-                    break;
+                        if difference >= 1 && difference <= 3 {
+                            continue;
+                        } else {
+                            safe = false;
+                            break;
+                        }
+                    } else {
+                        safe = false;
+                        break;
+                    }
                 }
-            } else {
-                is_monotonically_increasing = false;
-                break;
+                return safe;
             }
-        }
+            (a, b) if a > b => {
+                let mut safe = true;
+                for (current, next) in self.levels.iter().zip(self.levels.iter().skip(1)) {
+                    if current > next {
+                        // Now check that the increase is gradual
+                        let difference = current - next;
 
-        // The levels are all decreasing
-        let mut is_monotonically_decreasing = true;
-
-        for (current, next) in self.levels.iter().zip(self.levels.iter().skip(1)) {
-            if current > next {
-                // Now check that the increase is gradual
-                let difference = current - next;
-
-                if difference >= 1 && difference <= 3 {
-                    continue;
-                } else {
-                    is_gradual = false;
-                    break;
+                        if difference >= 1 && difference <= 3 {
+                            continue;
+                        } else {
+                            safe = false;
+                            break;
+                        }
+                    } else {
+                        safe = false;
+                        break;
+                    }
                 }
-            } else {
-                is_monotonically_decreasing = false;
-                break;
+
+                return safe;
             }
-        }
+            _ => false,
+        };
 
-        if (is_monotonically_increasing || is_monotonically_decreasing) && is_gradual {
-            return true;
-        } else {
-            return false;
-        }
+        return is_safe;
+    }
 
-        // Any two adjacent levels differ by at least one and at most three.
+    //Removes a single unsafe condition
+    fn is_safe_after_dampener(&mut self) -> Self {
+        match (&self.levels[0], &self.levels[1]) {
+            (a, b) if a < b => {
+                println!("increase");
+                let mut index = 0;
+                // Iterate through the vector using enumerate to get both index and element
+                while index < self.levels.len() - 1 {
+                    let current = &self.levels[index];
+                    let next = &self.levels[index + 1];
+
+                    let difference = next - current;
+                    println!("{}", difference);
+                    // Check if the current element is greater than the next
+                    // this makes the report unsafe since the levels should
+                    // increase in value
+                    if current > next || (difference < 1 || difference > 3) {
+                        println!("Got here");
+                        // Remove the element at the next index (index + 1)
+                        self.levels.remove(index);
+
+                        // break when a item is removed.
+                        break;
+
+                        // After removal, we don't increment `index` since we want to check the next element
+                        // in the same position (as the next element was shifted).
+                    } else {
+                        // If not, just move to the next index
+                        index += 1;
+                    }
+                }
+            }
+            (a, b) if a > b => {
+                println!("decrease");
+                let mut index = 0;
+                // Iterate through the vector using enumerate to get both index and element
+                while index < self.levels.len() - 1 {
+                    let current = &self.levels[index];
+                    let next = &self.levels[index + 1];
+
+                    let difference = current - next;
+
+                    println!("{}", difference);
+
+                    // Check if the current element is greater than the next
+                    // this makes the report unsafe since the levels should
+                    // increase in value
+                    if current < next || (difference < 1 || difference > 3) {
+                        // Remove the element at the next index (index + 1)
+                        self.levels.remove(index);
+
+                        // break when a item is removed.
+                        break;
+
+                        // After removal, we don't increment `index` since we want to check the next element
+                        // in the same position (as the next element was shifted).
+                    } else {
+                        // If not, just move to the next index
+                        index += 1;
+                    }
+                }
+            }
+            (a, b) if a == b => {
+                self.levels.remove(0);
+            }
+            _ => {}
+        };
+
+        return self.clone();
     }
 }
 
@@ -83,15 +152,30 @@ pub fn run_solution() {
     // Process the input
     let input_string = puzzle_input_string("2024", "02");
 
-    let reports: Vec<Report> = input_string
+    let reports_part_one = input_string
         .lines()
         .flat_map(|rep| str::parse(rep))
         .filter(|x: &Report| x.is_safe())
-        .inspect(|x| println!("{:?}", x))
-        .collect();
+        .count();
+
+    let reports_part_two = input_string
+        .lines()
+        .flat_map(|rep| str::parse(rep))
+        .map(|mut report: Report| {
+            // If the report is not safe we can remove the first unsafe level
+            if !report.is_safe() {
+                println!("Before mutation: {:?}", report);
+                report.is_safe_after_dampener(); // Mutate the report
+                println!("After mutation: {:?}", report);
+            }
+            report // Return the mutated report
+        })
+        .inspect(|x| println!("{:?}", x.is_safe()))
+        .filter(|x| x.is_safe()) // Filter the reports after mutation
+        .count();
 
     //Maybe return this and have the main function print it out.
-    println!("Part One Answer: {}", reports.len());
+    println!("Part One Answer: {}", reports_part_one);
     // Interpret input instructions part two
-    println!("Part Two Answer: {}", "");
+    println!("Part Two Answer: {}", reports_part_two);
 }
